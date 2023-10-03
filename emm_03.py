4138fa15a78eebb92bcfcd2d3c68cc5d2c82bab2
Mikhail <newlevelcenter@gmail.com>
2023-10-03 14:38:55 +0300

    task03

--- emm_03.py
+++ emm_03.py
@@ -4,7 +4,7 @@ import matplotlib
 
 matplotlib.rcParams.update({'font.size': 12})
 
-x = np.linspace(0, 10, 100)
+x = np.linspace(0, 30, 100)
 x1 = []
 for i in range(100):
     x1.append(3)
@@ -13,21 +13,22 @@ y1 = np.linspace(0, 10, 100)
 
 f1 = 2*x
 f2 = (1/4)*x
-f3 = 1-x
+f3 = 24-x
 
 
 # вказуємо в аргументі label текст легенди
-plt.plot(x, f1, ':b', label='$x_2>2x_1$')
+plt.plot(x, f1, ':b', label='$x_2<2x_1$')
 plt.plot(x, f2, '--b', label='$x_2>\\frac{1}{4}x_1$')
-plt.plot(x, f3, 'y', label='$x_2>1-x_1$')
+plt.plot(x, f3, 'y', label='$x_2<24-x_1$')
 
+
+plt.arrow (x= 0 , y= 0 , dx=4 , dy= 2, facecolor= 'red',edgecolor= 'none', width= .18 )
 
 plt.xlabel(r'$x1$', fontsize=16)
 plt.ylabel(r'$x2$', fontsize=16)
 
-plt.xlim([0, 2])
-plt.ylim([0, 2])
-
+plt.xlim([0, 30])
+plt.ylim([0, 30])
 
 
 # виводимо легенду
