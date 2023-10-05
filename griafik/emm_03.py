2c91aedac5dafc84f6bd7a4c93f66dd3c48c6546
Mikhail <newlevelcenter@gmail.com>
2023-10-05 14:39:24 +0300

    task03

--- griafik/emm_03.py
+++ griafik/emm_03.py
@@ -5,6 +5,7 @@ import matplotlib
 matplotlib.rcParams.update({'font.size': 12})
 
 x = np.linspace(0, 30, 100)
+
 x1 = []
 for i in range(100):
     x1.append(3)
@@ -12,17 +13,24 @@ y1 = np.linspace(0, 10, 100)
 
 
 f1 = 2*x
-f2 = (1/4)*x
+f2 = 0.5*x
 f3 = 24-x
 
-
 # вказуємо в аргументі label текст легенди
-plt.plot(x, f1, ':b', label='$x_2<2x_1$')
-plt.plot(x, f2, '--b', label='$x_2>\\frac{1}{4}x_1$')
-plt.plot(x, f3, 'y', label='$x_2<24-x_1$')
+plt.plot(x, f1, '--y', label='$x_2<2x_1$')
+plt.plot(x, f2, '--b', label='$x_2>0.5x_1$')
+plt.plot(x, f3, '--g', label='$x_2<24-x_1$')
+
+plt.scatter(15.9, 8.1, marker='s')
+plt.scatter(7.9, 16.1, marker='s')
+plt.scatter(0, 0, marker='^')
 
 
-plt.arrow (x= 0 , y= 0 , dx=4 , dy= 2, facecolor= 'red',edgecolor= 'none', width= .18 )
+
+
+
+
+
 
 plt.xlabel(r'$x1$', fontsize=16)
 plt.ylabel(r'$x2$', fontsize=16)
@@ -30,8 +38,6 @@ plt.ylabel(r'$x2$', fontsize=16)
 plt.xlim([0, 30])
 plt.ylim([0, 30])
 
-
-# виводимо легенду
 plt.legend(fontsize=14)
 plt.grid()
 plt.tight_layout()
